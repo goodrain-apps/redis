2ba666dea537a76e2059203390c51cb7a485c59b
zjhgoodrain <38780264+zjhgoodrain@users.noreply.github.com>
2018-06-20 23:09:04 +0800

    Update start.sh

--- 3.2.x/start.sh
+++ 3.2.x/start.sh
@@ -1,14 +1,18 @@
-
 #!/bin/bash
 
 set -xe
 
 sed -i "s/PORT/${PORT}/g" /usr/local/etc/redis/redis.conf
+sed -i "s/REDIS_PASS/${REDIS_PASS}/g" /usr/local/etc/redis/redis.conf
+sed -i "s/TCP_KEEPALIVE/${TCP_KEEPALIVE}/g" /usr/local/etc/redis/redis.conf
+sed -i "s/TIMEOUT/${TIMEOUT}/g" /usr/local/etc/redis/redis.conf
+sed -i "s/LOGLEVEL/${LOGLEVEL}/g" /usr/local/etc/redis/redis.conf
+sed -i "s/DATABASES/${DATABASES}/g" /usr/local/etc/redis/redis.conf
+sed -i "s/SLOWLOG_LOG_SLOWER_THAN/${SLOWLOG_LOG_SLOWER_THAN}/g" /usr/local/etc/redis/redis.conf
+sed -i "s/SLOWLOG_MAX_LEN/${SLOWLOG_MAX_LEN}/g" /usr/local/etc/redis/redis.conf
+sed -i "s/MAXCLIENTS/${MAXCLIENTS}/g" /usr/local/etc/redis/redis.conf
+sed -i "s/DBFILENAME/${DBFILENAME}/g" /usr/local/etc/redis/redis.conf
 
-if [ ! -z "$REDIS_PASS" ];then
-    echo "set redis password"
-    sed -i "s/# requirepass REDIS_PASS/requirepass ${REDIS_PASS}/g" /usr/local/etc/redis/redis.conf
-fi
 if [ "$1" = "/bin/bash" ];then
     exec /bin/bash
 else
