4d3e9b67272cc82d4cffcd92ba11883f50cc1948
barnettZQG <576501057@qq.com>
2020-05-21 18:58:26 +0800

    Update start.sh

--- 4.0/start.sh
+++ 4.0/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "set redis config"
 chmod +x /usr/bin/env2config
-env2config config -f /usr/local/etc/redis/redis.conf
+env2config conversion -f /usr/local/etc/redis/redis.conf
 redis-server /usr/local/etc/redis/redis.conf
